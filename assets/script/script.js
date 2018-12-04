b58be09ba2731a3838e13520cd8342d673401942
Difa Sanditya <difasanditya@gmail.com>
2018-12-04 10:21:57 +0700

    Finishing about section and added new section
    Change Understanding Armour to About Armour, added 3 new sections (Armour's Member, Our Project, and Say Hello).

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,4 +9,10 @@ $(document).ready(function(){
         backDelay: 2750,
         cursorChar: "_"
     });
+    $('textarea').each(function () {
+        this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+    }).on('input', function () {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
 })
