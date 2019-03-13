c44b0d5e729617a337f2bdd0e83dbfab88cca127
Difa Sanditya <difasanditya@gmail.com>
2019-03-13 23:18:45 +0700

    Added Sound to Armour Spelling
    Added sound to Armour spelling and remove some css for 000webhost.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    armourSound = new Audio("assets/music/armour.mp3");
     document.querySelector("#console").fakeScroll();
     var typed = new Typed('#typed', {
         strings: ["Hello World!", "Welcome to Armour Website", "Scroll down to continue!", "Or click the link above", "Or do whatever you want^800, lol"],
@@ -9,10 +10,13 @@ $(document).ready(function(){
         backDelay: 2750,
         cursorChar: "_"
     });
-    $('textarea').each(function () {
-        this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
-    }).on('input', function () {
-        this.style.height = 'auto';
-        this.style.height = (this.scrollHeight) + 'px';
+    $("textarea").each(function () {
+        this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+    }).on("input", function () {
+        this.style.height = "auto";
+        this.style.height = (this.scrollHeight) + "px";
+    });
+    $("#pronounce").click(function() {
+        armourSound.play();
     });
 })
