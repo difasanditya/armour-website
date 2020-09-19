269dc0e23cce144a1c03723e9f57b5659509e7b5
Difa Sanditya <difasanditya@gmail.com>
2020-09-19 20:36:49 +0700

    Fix indentation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-    armourSound = new Audio("assets/music/armour.mp3");
-    document.querySelector("#console").fakeScroll();
-    var typed = new Typed('#typed', {
-        strings: ["Hello World!", "Welcome to Armour Website", "Scroll down to continue!", "Or click the link above", "Or do whatever you want^800, lol"],
-        loop: true,
-        loopCount: Infinity,
-        typeSpeed: 35,
-        backSpeed: 35,
-        backDelay: 2750,
-        cursorChar: "_"
-    });
-    $("textarea").each(function () {
-        this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
-    }).on("input", function () {
-        this.style.height = "auto";
-        this.style.height = (this.scrollHeight) + "px";
-    });
-    $("#pronounce").click(function() {
-        armourSound.play();
-    });
+	armourSound = new Audio("assets/music/armour.mp3");
+	document.querySelector("#console").fakeScroll();
+	var typed = new Typed('#typed', {
+		strings: ["Hello World!", "Welcome to Armour Website", "Scroll down to continue!", "Or click the link above", "Or do whatever you want^800, lol"],
+		loop: true,
+		loopCount: Infinity,
+		typeSpeed: 35,
+		backSpeed: 35,
+		backDelay: 2750,
+		cursorChar: "_"
+	});
+	$("textarea").each(function () {
+		this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+	}).on("input", function () {
+		this.style.height = "auto";
+		this.style.height = (this.scrollHeight) + "px";
+	});
+	$("#pronounce").click(function() {
+		armourSound.play();
+	});
 })
